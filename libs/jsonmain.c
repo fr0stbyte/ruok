7402a2f5d9bac4840dbf40d4c0bc942958d71b1a
Radu Brumariu <brum76@gmail.com>
2011-05-19 08:17:55 -0700

    removed libtool m4s + added license in jsonmain.c

--- libs/jsonmain.c
+++ libs/jsonmain.c
@@ -1,20 +1,19 @@
 /*
-    jsonval - validates JSON files for well-formedness
-    Copyright (C) 2007 Ben Spencer
-
-    This program is free software; you can redistribute it and/or modify
+    This file is part of ruok - a program that measures timings of transferring data with URL syntax
+    Copyright (C) 2011 by Radu Brumariu [brum76@gmail.com]
+    
+    ruok is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
+    the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    ruok is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    along with ruok.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
